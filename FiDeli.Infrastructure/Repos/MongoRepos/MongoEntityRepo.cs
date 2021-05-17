19821544154d61e307b0d88531d762085defafb8
nambonumestinvisibili <dawid.holewa.dh@gmail.com>
2021-05-17 22:33:21 +0200

    created mongodb implementation of database, created test project

---
+++ FiDeli.Infrastructure/Repos/MongoRepos/MongoEntityRepo.cs
@@ -0,0 +1,70 @@
+﻿using FiDeli.Domain;
+using FiDeli.Domain.SeedWork;
+using MongoDB.Driver;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiDeli.Infrastructure.Repos.MongoRepos
+{
+    public abstract class MongoEntityRepo<T> : IEntityRepo<T> where T : Entity
+    {
+        public IMongoDatabase MongoDatabase { get; }
+        protected IMongoCollection<T> _collection;
+        public virtual string GetCollectionName()
+        {
+            return nameof(T);
+        }
+
+        public MongoEntityRepo(IMongoClient client)
+        {
+            MongoDatabase = client.GetDatabase("mydb");
+            _collection = MongoDatabase.GetCollection<T>(GetCollectionName());
+        }
+
+        public async Task Add(T item)
+        {
+            await _collection.InsertOneAsync(item);
+        }
+
+        public async Task Delete(T item)
+        {
+            await _collection.DeleteOneAsync(x => x.Id == item.Id);
+        }
+
+        public async Task<ICollection<T>> FindAll()
+        {
+            var res = await _collection.FindAsync<T>(_ => true);
+            return (ICollection<T>) res.ToListAsync();
+        }
+
+        public async Task<T> FindById(Guid id)
+        {
+            var res = await _collection.FindAsync(x => x.Id == id);
+            return res.FirstOrDefault();
+        }
+
+        public async Task Update(T item)
+        {
+            await _collection.FindOneAndReplaceAsync(x => x.Id == item.Id, item);
+        }
+    }
+
+    public abstract class MongoLocalisableRepo<T> :
+        MongoEntityRepo<T>,
+        ILocalisableRepo<T> where T : Localisable
+    {
+        protected MongoLocalisableRepo(IMongoClient client) : base(client)
+        {
+        }
+
+        public async Task<T> FindCloseToTheLocation(GeoLocation location)
+        {
+            var res = await _collection.FindAsync<T>(x => x.CurrentLocation == location);
+            return res.FirstOrDefault();
+        }
+    }
+}
---
+++ FiDeli.Infrastructure/Repos/MongoRepos/MongoRepositories.cs
@@ -0,0 +1,49 @@
+﻿using FiDeli.Domain;
+using FiDeli.Domain.Core.Commissions;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiDeli.Infrastructure.Repos.MongoRepos
+{
+    public class MongoCommissionerRepo :
+        MongoLocalisableRepo<Commissioner>,
+        ILocalisableRepo<Commissioner>
+    {
+        public MongoCommissionerRepo(IMongoClient client) : base(client)
+        {
+        }
+
+    }
+
+    public class MongoCommissionRepo :
+        MongoEntityRepo<Commission>,
+        IEntityRepo<Commission>
+    {
+        public MongoCommissionRepo(IMongoClient client) : base(client)
+        {
+        }
+
+    }
+
+    public class MongoDelivererRepo :
+        MongoLocalisableRepo<Deliverer>,
+        ILocalisableRepo<Deliverer>
+    {
+        public MongoDelivererRepo(IMongoClient client) : base(client)
+        {
+        }
+    }
+
+    public class MongoRecipientRepo :
+        MongoLocalisableRepo<Recipient>,
+        ILocalisableRepo<Recipient>
+    {
+        public MongoRecipientRepo(IMongoClient client) : base(client)
+        {
+        }
+    }
+}
